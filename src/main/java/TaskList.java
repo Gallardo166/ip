c88502d24ffb89aa6d06e4402dbf79d47aeaee41
Gallardo <75010545+Gallardo166@users.noreply.github.com>
2026-01-23 11:55:23 +0800

    Handle invalid task index

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,34 +1,46 @@
 public class TaskList {
   private Task[] tasks = new Task[100];
-  private int latestId = 0;
+  private int latestId = -1;
 
   public void add(Task task) {
-    tasks[latestId] = task;
     latestId += 1;
+    tasks[latestId] = task;
   }
 
-  public void markTask(int index) {
-    tasks[index].markAsDone();
+  public void markTask(int index) throws BibotException {
+    if (index < 0 || index > latestId) {
+      throw new BibotException("There's no such task!");
+    } else {
+      tasks[index].markAsDone();
+    }
   }
 
-  public void unmarkTask(int index) {
-    tasks[index].markAsUndone();
+  public void unmarkTask(int index) throws BibotException {
+    if (index < 0 || index > latestId) {
+      throw new BibotException("There's no such task!");
+    } else {
+      tasks[index].markAsUndone();
+    }
   }
 
-  public Task get(int index) {
-    return tasks[index]; 
+  public Task get(int index) throws BibotException {
+    if (index < 0 || index > latestId) {
+      throw new BibotException("There's no such task!");
+    } else {
+      return tasks[index]; 
+    }
   }
 
   public void display() {
-    for (int i = 0; i < latestId; i++) {
+    for (int i = 0; i <= latestId; i++) {
       System.out.printf("      %d. %s\n", i + 1, tasks[i]);
     }
   }
 
   public void printLength() {
-    String taskNum = latestId == 1
+    String taskNum = latestId == 0
                      ? "1 task"
-                     : String.format("%d tasks", latestId);
+                     : String.format("%d tasks", latestId + 1);
     System.out.printf("     Now you have %s in the list.\n", taskNum);
   }
 }
