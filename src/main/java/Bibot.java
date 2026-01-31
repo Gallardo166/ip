802b8d3dc600f859c5290245e24493343cd3b9b8
Gallardo <75010545+Gallardo166@users.noreply.github.com>
2026-01-31 23:21:51 +0800

    Add Storage class

--- src/main/java/Bibot.java
+++ src/main/java/Bibot.java
@@ -1,57 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import java.util.Scanner;
-
 public class Bibot {
-    public static TaskList loadTasks() {
-        TaskList taskList = new TaskList();
-        try {
-            File f = new File("./data/bibot.txt");
-            Scanner s = new Scanner(f);
-            while (s.hasNext()) {
-                String[] splitLine = s.nextLine().split(" \\| ");
-                String taskType = splitLine[0];
-                boolean isDone = splitLine[1].equals("completed");
-                if (taskType.equals("T")) {
-                    taskList.add(new ToDo(splitLine[2], isDone));
-                } else if (taskType.equals("D")) {
-                    taskList.add(new Deadline(splitLine[2], splitLine[3], isDone));
-                } else {
-                    taskList.add(new Event(splitLine[2], splitLine[3], splitLine[4], isDone));
-                }
-            }
-            s.close();
-            return taskList;
-        } catch (FileNotFoundException | BibotException e) {
-            return taskList;
-        }
-    }
-
-    public static void saveTasks(TaskList taskList) {
-        try {
-            //@@author Gallardo166-reused
-            //Reused from https://stackoverflow.com/questions/28947250/create-a-directory-if-it-does-not-exist-and-then-create-the-files-in-that-direct
-            // with minor modifications
-            File directory = new File("./data");
-            if (!directory.exists()) {
-                directory.mkdir();
-            }
-            //@@author
-            FileWriter fw = new FileWriter("./data/bibot.txt");
-            for (Task task : taskList) {
-               fw.write(task.fileString() + "\n");
-            }
-            fw.close();
-        } catch (IOException e) {  
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-    }
-
     public static void main(String[] args) {
-        TaskList taskList = loadTasks();
+        Storage storage = new Storage("./data/bibot.txt");
+        TaskList taskList = storage.loadTasks();
         Ui ui = new Ui();
 
         ui.displayTopLine();
@@ -80,14 +30,14 @@ public class Bibot {
                     // https://stackoverflow.com/questions/5585779/how-do-i-convert-a-string-to-an-int-in-java
                     int index = Integer.parseInt(command.split(" ")[1]) - 1;
                     taskList.markTask(index);
-                    saveTasks(taskList);
+                    storage.saveTasks(taskList);
                     ui.displayMessage("Nice! I've marked this task as done:");
                     ui.displayTask(taskList.get(index));
 
                 } else if (command.startsWith("unmark ")) {
                     int index = Integer.parseInt(command.split(" ")[1]) - 1;
                     taskList.unmarkTask(index);
-                    saveTasks(taskList);
+                    storage.saveTasks(taskList);
                     ui.displayMessage("OK, I've marked this task as not done yet:");
                     ui.displayTask(taskList.get(index));
 
@@ -98,7 +48,7 @@ public class Bibot {
                         String description = command.replaceFirst("todo +", "");
                         ToDo todo = new ToDo(description);
                         taskList.add(todo);
-                        saveTasks(taskList);
+                        storage.saveTasks(taskList);
                         ui.displayMessage("Got it. I've added this task:");
                         ui.displayTask(todo);
                         taskList.printLength();
@@ -113,7 +63,7 @@ public class Bibot {
                         String date = splitCommand[1];
                         Deadline deadline = new Deadline(description, date);
                         taskList.add(deadline);
-                        saveTasks(taskList);
+                        storage.saveTasks(taskList);
                         ui.displayMessage("Got it. I've added this task:");
                         ui.displayTask(deadline);
                         taskList.printLength();
@@ -130,7 +80,7 @@ public class Bibot {
                         String endDate = splitCommand[1].split(" /to ")[1];
                         Event event = new Event(description, startDate, endDate);
                         taskList.add(event);
-                        saveTasks(taskList);
+                        storage.saveTasks(taskList);
                         ui.displayMessage("Got it. I've added this task:");
                         ui.displayTask(event);
                         taskList.printLength();
@@ -143,7 +93,7 @@ public class Bibot {
                     } else {
                         int index = Integer.parseInt(command.split(" +")[1]) - 1;
                         Task deletedTask = taskList.deleteTask(index);
-                        saveTasks(taskList);
+                        storage.saveTasks(taskList);
                         ui.displayMessage("Noted. I've removed this task:");
                         ui.displayTask(deletedTask);
                         taskList.printLength();
