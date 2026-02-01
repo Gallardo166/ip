59014737e0924a2bc64277ec9a93eccbb0f5a2be
Gallardo <75010545+Gallardo166@users.noreply.github.com>
2026-02-01 13:21:18 +0800

    Remove latestId field from TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,15 +3,13 @@ import java.util.Iterator;
 
 public class TaskList implements Iterable<Task> {
     private ArrayList<Task> tasks = new ArrayList<>();
-    private int latestId = -1;
 
     public void add(Task task) {
-        latestId += 1;
         tasks.add(task);
     }
 
     public void markTask(int index) throws BibotException {
-        if (index < 0 || index > latestId) {
+        if (index < 0 || index >= tasks.size()) {
             throw new BibotException("There's no such task!");
         } else {
             tasks.get(index).markAsDone();
@@ -19,7 +17,7 @@ public class TaskList implements Iterable<Task> {
     }
 
     public void unmarkTask(int index) throws BibotException {
-        if (index < 0 || index > latestId) {
+        if (index < 0 || index >= tasks.size()) {
             throw new BibotException("There's no such task!");
         } else {
             tasks.get(index).markAsUndone();
@@ -27,16 +25,15 @@ public class TaskList implements Iterable<Task> {
     }
 
     public Task deleteTask(int index) throws BibotException {
-        if (index < 0 || index > latestId) {
+        if (index < 0 || index >= tasks.size()) {
             throw new BibotException("There's no such task!");
         } else {
-            latestId -= 1;
             return tasks.remove(index);
         }
     }
 
     public Task get(int index) throws BibotException {
-        if (index < 0 || index > latestId) {
+        if (index < 0 || index >= tasks.size()) {
             throw new BibotException("There's no such task!");
         } else {
             return tasks.get(index);
@@ -44,7 +41,7 @@ public class TaskList implements Iterable<Task> {
     }
 
     public int getLength() {
-        return latestId + 1;
+        return tasks.size(); 
     }
 
     public Iterator<Task> iterator() {
