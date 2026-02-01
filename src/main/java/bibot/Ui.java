b680208f6bc5e01103e83f0154e2dc85bc7376de
Gallardo <75010545+Gallardo166@users.noreply.github.com>
2026-02-01 20:02:22 +0800

    Add JavaDoc comments to classes in bibot package

--- src/main/java/bibot/Ui.java
+++ src/main/java/bibot/Ui.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 import bibot.task.Task;
 import bibot.task.TaskList;
 
+/**
+ * Represents component that handles output display to user.
+ */
 public class Ui {
     // Solution below inspired by https://www.w3schools.com/java/java_user_input.asp
     private static Scanner commandScanner = new Scanner(System.in);
@@ -13,18 +16,36 @@ public class Ui {
     private String taskIndentation = " ".repeat(6);
     private String divider = "_".repeat(39);
 
+    /**
+     * Reads the user input.
+     * Reads up to a newline.
+     * 
+     * @return User input as a <code>String</code>.
+     */
     public String readInput() {
         return commandScanner.nextLine();
     }
 
+    /**
+     * Prints the specified text with message-level indentation (5).
+     */
     public void displayMessage(String text) {
        System.out.println(messageIndentation + text); 
     }
     
+    /**
+     * Prints the specified <code>Task</code> as a 
+     * <code>String</code> with task-level indentation (6).
+     */
     public void displayTask(Task task) {
         System.out.println(taskIndentation + task);
     }
 
+    /**
+     * Prints numbered list of <code>Task</code>s as
+     * <code>String</code>s in the specified <code>TaskList</code>
+     * with task-level indentation (6).
+     */
     public void displayTaskList(TaskList taskList) {
         int taskNumber = 1;
         for (Task task: taskList) {
@@ -34,6 +55,10 @@ public class Ui {
         }
     }
     
+    /**
+     * Prints a message containing the number of <code>Task</code>s
+     * in the specified <code>TaskList</code>.
+     */
     public void displayTaskCount(TaskList taskList) {
         int numTasks = taskList.getLength();
         String taskString = numTasks == 1
@@ -43,14 +68,23 @@ public class Ui {
                 + numTasks + " " + taskString + " in the list.");
     }
 
+    /**
+     * Prints a horizontal divider without an extra newline.
+     */
     public void displayTopLine() {
         System.out.println(dividerIndentation + divider);
     }
 
+    /**
+     * Prints a horizontal divider with an extra newline.
+     */
     public void displayBottomLine() {
         System.out.println(dividerIndentation + divider + "\n");
     }
 
+    /**
+     * Prints a greeting to the user.
+     */
     public void displayGreeting() {
         this.displayTopLine();
         this.displayMessage("Hello! I'm Bibot!");
@@ -58,6 +92,9 @@ public class Ui {
         this.displayBottomLine();
     }
 
+    /**
+     * Stops the <code>Ui</code> from reading input.
+     */
     public void end() {
         commandScanner.close();
     }
